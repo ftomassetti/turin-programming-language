505c9f25b9c6d87f64c63b92c180157ceaba563b
Federico Tomassetti <federico@tomassetti.me>
2015-09-06 13:05:09 +0200

    support missing opcodes

--- turin-compiler/src/main/java/me/tomassetti/turin/compiler/bytecode/MathOperationBytecode.java
+++ turin-compiler/src/main/java/me/tomassetti/turin/compiler/bytecode/MathOperationBytecode.java
@@ -16,6 +16,15 @@ public class MathOperationBytecode extends BytecodeSequence {
                     case INT:
                         opcode = Opcodes.IMUL;
                         break;
+                    case LONG:
+                        opcode = Opcodes.LMUL;
+                        break;
+                    case FLOAT:
+                        opcode = Opcodes.FMUL;
+                        break;
+                    case DOUBLE:
+                        opcode = Opcodes.DMUL;
+                        break;
                     default:
                         throw new UnsupportedOperationException(operandsType.name());
                 }
@@ -24,6 +33,51 @@ public class MathOperationBytecode extends BytecodeSequence {
                 switch (operandsType) {
                     case INT:
                         opcode = Opcodes.IADD;
+                        break;
+                    case LONG:
+                        opcode = Opcodes.LADD;
+                        break;
+                    case FLOAT:
+                        opcode = Opcodes.FADD;
+                        break;
+                    case DOUBLE:
+                        opcode = Opcodes.DADD;
+                        break;
+                    default:
+                        throw new UnsupportedOperationException(operandsType.name());
+                }
+                break;
+            case SUBTRACTION:
+                switch (operandsType) {
+                    case INT:
+                        opcode = Opcodes.ISUB;
+                        break;
+                    case LONG:
+                        opcode = Opcodes.LSUB;
+                        break;
+                    case FLOAT:
+                        opcode = Opcodes.FSUB;
+                        break;
+                    case DOUBLE:
+                        opcode = Opcodes.DSUB;
+                        break;
+                    default:
+                        throw new UnsupportedOperationException(operandsType.name());
+                }
+                break;
+            case DIVISION:
+                switch (operandsType) {
+                    case INT:
+                        opcode = Opcodes.IDIV;
+                        break;
+                    case LONG:
+                        opcode = Opcodes.LDIV;
+                        break;
+                    case FLOAT:
+                        opcode = Opcodes.FDIV;
+                        break;
+                    case DOUBLE:
+                        opcode = Opcodes.DDIV;
                         break;
                     default:
                         throw new UnsupportedOperationException(operandsType.name());
