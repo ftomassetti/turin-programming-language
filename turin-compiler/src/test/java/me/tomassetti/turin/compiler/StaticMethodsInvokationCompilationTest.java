6078759a1b1ad111fe0d2d7dbb9b706126b2d3ad
Federico Tomassetti <federico@tomassetti.me>
2015-09-14 22:27:56 +0200

    add test StaticMethodsInvokationCompilationTest

---
+++ turin-compiler/src/test/java/me/tomassetti/turin/compiler/StaticMethodsInvokationCompilationTest.java
@@ -0,0 +1,49 @@
+package me.tomassetti.turin.compiler;
+
+import com.google.common.collect.ImmutableList;
+import me.tomassetti.turin.TurinClassLoader;
+import me.tomassetti.turin.parser.Parser;
+import me.tomassetti.turin.parser.analysis.resolvers.ComposedResolver;
+import me.tomassetti.turin.parser.analysis.resolvers.InFileResolver;
+import me.tomassetti.turin.parser.analysis.resolvers.Resolver;
+import me.tomassetti.turin.parser.analysis.resolvers.SrcResolver;
+import me.tomassetti.turin.parser.ast.TurinFile;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class StaticMethodsInvokationCompilationTest {
+
+    private Resolver getResolverFor(TurinFile turinFile) {
+        return new ComposedResolver(ImmutableList.of(new InFileResolver(), new SrcResolver(ImmutableList.of(turinFile))));
+    }
+
+    @Test
+    public void compileInvokationsToCollectionsEmptyList() throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException, IOException {
+        TurinFile turinFile = new Parser().parse(this.getClass().getResourceAsStream("/static_methods_invokation.to"));
+
+        // generate bytecode
+        Compiler instance = new Compiler(getResolverFor(turinFile), new Compiler.Options());
+        List<ClassFileDefinition> classFileDefinitions = instance.compile(turinFile);
+        assertEquals(1, classFileDefinitions.size());
+
+        TurinClassLoader turinClassLoader = new TurinClassLoader();
+        Class functionClass = turinClassLoader.addClass(classFileDefinitions.get(0).getName(),
+                classFileDefinitions.get(0).getBytecode());
+        assertEquals(0, functionClass.getConstructors().length);
+
+        Method invoke = functionClass.getMethod("invoke", int.class, int.class);
+        Object result = invoke.invoke(null);
+        assertTrue(result instanceof List);
+        List list = (List)result;
+        assertTrue(list.isEmpty());
+    }
+
+}
+
