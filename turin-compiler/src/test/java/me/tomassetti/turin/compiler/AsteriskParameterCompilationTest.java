3b60e8758a0f163f12493264e3e6bd24569b5eb4
Federico Tomassetti <federico@tomassetti.me>
2015-09-26 17:48:23 +0200

    add test for using asterisk with an object

--- turin-compiler/src/test/java/me/tomassetti/turin/compiler/AsteriskParameterCompilationTest.java
+++ turin-compiler/src/test/java/me/tomassetti/turin/compiler/AsteriskParameterCompilationTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -84,6 +83,90 @@ public class AsteriskParameterCompilationTest extends AbstractCompilerTest {
 
         EasyMock.verify(errorCollector);
     }
+
+
+    @Test
+    public void correctUsageOfAsteriskOnCreationWithObject() throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException, IOException {
+        TurinFile turinFile = new Parser().parse(this.getClass().getResourceAsStream("/ok_usage_of_asterisk_in_creation_with_object.to"));
+
+        // generate bytecode
+        Compiler.Options options = new Compiler.Options();
+        Compiler instance = new Compiler(getResolverFor(turinFile), options);
+
+        List<ClassFileDefinition> classFileDefinitions = instance.compile(turinFile, new MyErrorCollector());
+
+        TurinClassLoader turinClassLoader = new TurinClassLoader();
+        Class typeClass = turinClassLoader.addClass(classFileDefinitions.get(0).getName(),
+                classFileDefinitions.get(0).getBytecode());
+        Class foo1 = turinClassLoader.addClass(classFileDefinitions.get(1).getName(),
+                classFileDefinitions.get(1).getBytecode());
+        Class foo2 = turinClassLoader.addClass(classFileDefinitions.get(2).getName(),
+                classFileDefinitions.get(2).getBytecode());
+        Class foo3 = turinClassLoader.addClass(classFileDefinitions.get(3).getName(),
+                classFileDefinitions.get(3).getBytecode());
+
+        Object result1 = foo1.getMethod("invoke").invoke(null);
+        Object result2 = foo2.getMethod("invoke").invoke(null);
+        Object result3 = foo3.getMethod("invoke").invoke(null);
+
+        assertEquals(1, typeClass.getMethod("getA").invoke(result1));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result1));
+        assertEquals(5, typeClass.getMethod("getC").invoke(result1));
+        assertEquals(7, typeClass.getMethod("getD").invoke(result1));
+
+        assertEquals(1, typeClass.getMethod("getA").invoke(result2));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result2));
+        assertEquals(15, typeClass.getMethod("getC").invoke(result2));
+        assertEquals(7, typeClass.getMethod("getD").invoke(result2));
+
+        assertEquals(1, typeClass.getMethod("getA").invoke(result3));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result3));
+        assertEquals(5, typeClass.getMethod("getC").invoke(result3));
+        assertEquals(15, typeClass.getMethod("getD").invoke(result3));
+    }
+
+    @Test
+    public void correctUsageOfAsteriskOnMethodWithObject() throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException, IOException {
+        TurinFile turinFile = new Parser().parse(this.getClass().getResourceAsStream("/ok_usage_of_asterisk_in_creation_with_object.to"));
+
+        // generate bytecode
+        Compiler.Options options = new Compiler.Options();
+        Compiler instance = new Compiler(getResolverFor(turinFile), options);
+
+        List<ClassFileDefinition> classFileDefinitions = instance.compile(turinFile, new MyErrorCollector());
+
+        TurinClassLoader turinClassLoader = new TurinClassLoader();
+        Class typeClass = turinClassLoader.addClass(classFileDefinitions.get(0).getName(),
+                classFileDefinitions.get(0).getBytecode());
+        Class callMe = turinClassLoader.addClass(classFileDefinitions.get(1).getName(),
+                classFileDefinitions.get(1).getBytecode());
+        Class foo1 = turinClassLoader.addClass(classFileDefinitions.get(2).getName(),
+                classFileDefinitions.get(2).getBytecode());
+        Class foo2 = turinClassLoader.addClass(classFileDefinitions.get(3).getName(),
+                classFileDefinitions.get(3).getBytecode());
+        Class foo3 = turinClassLoader.addClass(classFileDefinitions.get(4).getName(),
+                classFileDefinitions.get(4).getBytecode());
+
+        Object result1 = foo1.getMethod("invoke").invoke(null);
+        Object result2 = foo2.getMethod("invoke").invoke(null);
+        Object result3 = foo3.getMethod("invoke").invoke(null);
+
+        assertEquals(23, typeClass.getMethod("getA").invoke(result1));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result1));
+        assertEquals(5, typeClass.getMethod("getC").invoke(result1));
+        assertEquals(7, typeClass.getMethod("getD").invoke(result1));
+
+        assertEquals(1, typeClass.getMethod("getA").invoke(result2));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result2));
+        assertEquals(15, typeClass.getMethod("getC").invoke(result2));
+        assertEquals(7, typeClass.getMethod("getD").invoke(result2));
+
+        assertEquals(1, typeClass.getMethod("getA").invoke(result3));
+        assertEquals(10, typeClass.getMethod("getB").invoke(result3));
+        assertEquals(5, typeClass.getMethod("getC").invoke(result3));
+        assertEquals(7, typeClass.getMethod("getD").invoke(result3));
+    }
+
 
 }
 
