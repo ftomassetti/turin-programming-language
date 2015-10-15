bb93d34d39cbd19a4ce7dddf0214b5f4dfe9cc87
Federico Tomassetti <federico@tomassetti.me>
2015-10-15 15:51:15 +0200

    comment Symbol

--- turin-compiler/src/main/java/me/tomassetti/turin/symbols/Symbol.java
+++ turin-compiler/src/main/java/me/tomassetti/turin/symbols/Symbol.java
@@ -3,7 +3,9 @@ package me.tomassetti.turin.symbols;
 import me.tomassetti.turin.parser.analysis.resolvers.SymbolResolver;
 import me.tomassetti.turin.parser.ast.typeusage.TypeUsage;
 
+/**
+ * Something generic: it could come from an AST or be a loaded value.
+ */
 public interface Symbol {
-
     TypeUsage calcType(SymbolResolver resolver);
 }
